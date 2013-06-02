ffa44fdcb1757f83d0b9f5581ce36374df7f6f53
Christopher Wellons <mosquitopsu@gmail.com>
2013-06-02 17:04:57 -0400

    Abstract away WebGL programs.

--- src/DisplayGL.js
+++ src/DisplayGL.js
@@ -14,96 +14,23 @@ DisplayGL.prototype = Object.create(Display.prototype);
 DisplayGL.prototype.constructor = DisplayGL;
 
 /**
- * Synchronously fetch data from the server.
- * @param {string} url
- * @returns {string}
- */
-DisplayGL.fetch = function(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, false);
-    xhr.send();
-    return xhr.responseText;
-};
-
-/**
- * Compile a shader from a URL.
- * @param {number} type
- * @param {string} url
- * @returns {WebGLShader}
- */
-DisplayGL.prototype.makeShader = function(type, url) {
-    var gl = this.gl;
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, DisplayGL.fetch(url));
-    gl.compileShader(shader);
-    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        return shader;
-    } else {
-        throw new Error(gl.getShaderInfoLog(shader));
-    }
-};
-
-/**
- * Compile and link a program from a pair of shaders.
- * @param {string} vertUrl
- * @param {string} vergrafUrl
- * @returns {WebGLProgram}
- */
-DisplayGL.prototype.makeProgram = function(vertUrl, fragUrl) {
-    var gl = this.gl;
-    var program = gl.createProgram();
-    gl.attachShader(program, this.makeShader(gl.VERTEX_SHADER, vertUrl));
-    gl.attachShader(program, this.makeShader(gl.FRAGMENT_SHADER, fragUrl));
-    gl.linkProgram(program);
-    if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        return program;
-    } else {
-        throw new Error(gl.getProgramInfoLog(program));
-    }
-};
-
-/**
- * Set a uniform's data.
- * @param {string} name uniform variable name
- * @param {number|Point} value
- * @returns {DisplayGL} this
- */
-DisplayGL.prototype.uniform = function(name, value) {
-    var v = this.vars[name];
-    if (value instanceof Point) {
-        this.gl.uniform3f(v, value.x, value.y, value.z);
-    } else {
-        this.gl.uniform1f(v, value);
-    }
-    return this;
-};
-
-/**
  * Initialize this display.
  * @returns {DisplayGL} this
  */
 DisplayGL.prototype.init = function() {
-    var start = Date.now();
     var gl = this.gl;
-
     gl.enable(gl.BLEND);
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-    this.vars = {};
-    this.program = this.makeProgram('src/project.vert', 'src/color.frag');
+    this.programs = {};
+    this.programs.dots = new Program(gl, 'src/project.vert', 'src/dot.frag');
+    this.programs.dots.attrib('position')
+        .uniform('translate')
+        .uniform('rotate')
+        .uniform('focal')
+        .uniform('scale');
 
-    this.vars.position = gl.getAttribLocation(this.program, 'position');
-    gl.enableVertexAttribArray(this.vars.position);
-
-    this.vars.translate = gl.getUniformLocation(this.program, 'translate');
-    this.vars.rotate = gl.getUniformLocation(this.program, 'rotate');
-    this.vars.focal = gl.getUniformLocation(this.program, 'focal');
-    this.vars.scale = gl.getUniformLocation(this.program, 'scale');
-
-    gl.useProgram(this.program);
     this.dirty = true;
-    var time = (Date.now() - start) / 1000;
-    console.log('Initialization took ' + time + ' seconds.');
     return this;
 };
 
@@ -112,18 +39,24 @@ DisplayGL.prototype.init = function() {
  * @returns {DisplayGL} this
  */
 DisplayGL.prototype.clean = function() {
+    var start = Date.now();
+
     var gl = this.gl;
-    var buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     var array = new Float32Array(this.points.length * 3);
     for (var i = 0; i < this.points.length; i++) {
         array[3 * i + 0] = this.points[i].point.x;
         array[3 * i + 1] = this.points[i].point.y;
         array[3 * i + 2] = this.points[i].point.z;
     }
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ARRAY_BUFFER, array, gl.STATIC_DRAW);
-    gl.vertexAttribPointer(this.vars.position, 3, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(this.programs.dots.vars.position,
+                           3, gl.FLOAT, false, 0, 0);
     this.dirty = false;
+
+    var time = (Date.now() - start) / 1000;
+    console.log('Update took ' + time + ' seconds.');
     return this;
 };
 
@@ -145,13 +78,16 @@ DisplayGL.prototype.render = function() {
     var gl = this.gl;
     if (this.dirty) this.clean();
     this.clear();
-    this.uniform('translate', this.translate);
-    this.uniform('rotate', this.rotate);
-    this.uniform('focal', this.fl);
-    this.uniform('scale', this.scale * 2);
+
+    this.programs.dots.use()
+        .uniform('translate', this.translate)
+        .uniform('rotate', this.rotate)
+        .uniform('focal', this.fl)
+        .uniform('scale', this.scale * 2);
     gl.drawArrays(gl.POINTS, 0, this.points.length);
     if (gl.getError() !== gl.NO_ERROR) {
         throw new Error('WebGL rendering error');
     }
+
     return this;
 };
---
+++ src/Program.js
@@ -0,0 +1,84 @@
+/**
+ * Compile and link a program from a pair of shaders.
+ * @param {WebGLRenderingContext} gl
+ * @param {string} vertUrl
+ * @param {string} vergrafUrl
+ * @returns {WebGLProgram}
+ */
+function Program(gl, vertUrl, fragUrl) {
+    this.gl = gl;
+    this.program = gl.createProgram();
+    gl.attachShader(this.program, this.makeShader(gl.VERTEX_SHADER, vertUrl));
+    gl.attachShader(this.program, this.makeShader(gl.FRAGMENT_SHADER, fragUrl));
+    gl.linkProgram(this.program);
+    if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
+        throw new Error(gl.getProgramInfoLog(this.program));
+    }
+    this.vars = {};
+}
+
+/**
+ * Synchronously fetch data from the server.
+ * @param {string} url
+ * @returns {string}
+ */
+Program.fetch = function(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
+    return xhr.responseText;
+};
+
+/**
+ * Compile a shader from a URL.
+ * @param {number} type
+ * @param {string} url
+ * @returns {WebGLShader}
+ */
+Program.prototype.makeShader = function(type, url) {
+    var gl = this.gl;
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, Program.fetch(url));
+    gl.compileShader(shader);
+    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        return shader;
+    } else {
+        throw new Error(gl.getShaderInfoLog(shader));
+    }
+};
+
+/**
+ * Tell WebGL to use this program.
+ * @returns {Program} this
+ */
+Program.prototype.use = function() {
+    this.gl.useProgram(this.program);
+    return this;
+};
+
+/**
+ * Set a uniform's data.
+ * @param {string} name uniform variable name
+ * @param {number|Point} [value]
+ * @returns {DisplayGL} this
+ */
+Program.prototype.uniform = function(name, value) {
+    if (value == null) {
+        this.vars[name] = this.gl.getUniformLocation(this.program, name);
+    } else {
+        this.use();
+        var v = this.vars[name];
+        if (value instanceof Point) {
+            this.gl.uniform3f(v, value.x, value.y, value.z);
+        } else {
+            this.gl.uniform1f(v, value);
+        }
+    }
+    return this;
+};
+
+Program.prototype.attrib = function(name) {
+    this.vars[name] = this.gl.getAttribLocation(this.program, name);
+    this.gl.enableVertexAttribArray(this.vars.position);
+    return this;
+};
